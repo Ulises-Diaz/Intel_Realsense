e5ee6dec63fd12f2d60b221f98715ef427d2e767
Ulises-Diaz <A00837223@tec.mx>
2025-01-29 23:33:25 -0600

    Background removal

--- ros_env/build/intel_camera/build/lib/intel_camera/intel_sub.py
+++ ros_env/build/intel_camera/build/lib/intel_camera/intel_sub.py
@@ -8,12 +8,21 @@ class IntelSubscriber(Node) :
     def __init__ (self) : 
         super().__init__("Intel_subscriber")
         self.subscription_rgb = self.create_subscription(Image, "rgb_frame", self.rgb_frame_callback, 10)
+        self.subscription_bg_removed = self.create_subscription(Image, "bg_removed_frame", self.bg_removed_callback, 10)
+        
+        
         self.br_rgb = CvBridge()
 
     def rgb_frame_callback(self, data): 
         self.get_logger().warning("Receiving RGB frame")
         current_frame = self.br_rgb.imgmsg_to_cv2(data) 
         cv2.imshow("RGB", current_frame)
+        cv2.waitKey(1)
+
+    def bg_removed_callback(self, data): 
+        self.get_logger().info("Recieving Background-Removed Frame")
+        bg_removed_frame = self.br_rgb.imgmsg_to_cv2(data, desired_encoding= "rgb8")
+        cv2.imshow("Background removed Frame" , bg_removed_frame)
         cv2.waitKey(1)
 
 def main (args = None) : 
--- ros_env/src/intel_camera/intel_camera/intel_pub.py
+++ ros_env/src/intel_camera/intel_camera/intel_pub.py
@@ -11,16 +11,30 @@ import time
 
 class IntelPublisher(Node):
     def __init__(self):
-        super().__init__("Intel_publisher")  # Nombre corregido
-        self.intel_publisher_rgb = self.create_publisher(Image, "rgb_frame", 10)  # Publicador
+        super().__init__("Intel_publisher")  
+        self.intel_publisher_rgb = self.create_publisher(Image, "rgb_frame", 10)  # Publisher
+        self.intel_publisher_bg_removed = self.create_publisher(Image, "bg_removed_frame", 10)
         self.br_rgb = CvBridge()  # Convertidor de OpenCV a ROS
         
         try:
             # Configuración del pipeline
             self.pipe = rs.pipeline()
             self.cfg = rs.config()
+            
+            # Enable color and depth
             self.cfg.enable_stream(rs.stream.color, 640, 480, rs.format.bgr8, 15)  # Configuración optimizada
+            self.cfg.enable_stream(rs.stream.depth, 640, 480, rs.format.z16, 30)
+            
             self.pipe.start(self.cfg)
+            
+            # Depth Scale
+            self.depth_scale = self.pipe.get_active_profile().get_device().first_depth_sensor().get_depth_scale()
+            
+            # Set clipping distance (1 mts)
+            self.clipping_distance = 1.0 / self.depth_scale
+            
+            # Depth color
+            self.align = rs.align(rs.stream.color)
             
             # Hilo separado para el procesamiento de frames
             self.camera_thread = threading.Thread(target=self.start_camera, daemon=True)
@@ -36,22 +50,41 @@ class IntelPublisher(Node):
             while rclpy.ok():
                 start_time = time.time()
                 frames = self.pipe.wait_for_frames(5000)  # Tiempo de espera de 5 segundos
-                color_frame = frames.get_color_frame()
                 
-                if not color_frame:
+                # Align depth to color frame
+                aligned_frames = self.align.process(frames)
+                color_frame = aligned_frames.get_color_frame()
+                depth_frame = aligned_frames.get_depth_frame()
+                
+                if not color_frame or not depth_frame:
                     self.get_logger().warning("No se recibió un frame de color.")
                     continue
                 
                 # Procesamiento del frame
                 color_image = np.asanyarray(color_frame.get_data()) # Esto te lo da en formato BGR
+                depth_image = np.asanyarray(depth_frame.get_data())
                 
-                # Convertir a RGB
+                # Convertir a RGB para poder publicar
                 color_image_rgb = cv2.cvtColor(color_image, cv2.COLOR_BGR2RGB)
+                self.intel_publisher_rgb.publish(self.br_rgb.cv2_to_imgmsg(color_image_rgb, encoding='rgb8'))
                 
-                self.intel_publisher_rgb.publish(self.br_rgb.cv2_to_imgmsg(color_image_rgb, encoding = 'rgb8'))
+                # Aplicar background removal
+                bg_removed = self.background_removal(color_image, depth_image)
+                self.intel_publisher_bg_removed.publish(self.br_rgb.cv2_to_imgmsg(bg_removed, encoding='rgb8'))
+                
                 self.get_logger().info(f"Publishing RGB frame. Procesado en {time.time() - start_time:.3f} segundos")
         except Exception as e:
             self.get_logger().error(f"Error al capturar frames: {e}")
+
+    def background_removal (self, color_image, depth_image) : 
+        " Remueve el background con un threshold "
+        grey_color = 153
+        depth_image_3d = np.dstack((depth_image, depth_image, depth_image))
+        
+        # Aplicar background removal
+        bg_removed = np.where((depth_image_3d > self.clipping_distance) | (depth_image_3d <= 0), grey_color, color_image)
+        
+        return bg_removed.astype(np.uint8)
 
     def destroy_node(self):
         """Detiene el pipeline al cerrar el nodo."""
