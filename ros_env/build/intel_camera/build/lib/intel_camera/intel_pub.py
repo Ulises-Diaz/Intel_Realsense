781bea00aaa198bab9fb85cfdff6c307d0e5f390
Ulises-Diaz <A00837223@tec.mx>
2025-01-30 23:29:22 -0600

    Failed PC

--- ros_env/build/intel_camera/build/lib/intel_camera/intel_pub.py
+++ ros_env/build/intel_camera/build/lib/intel_camera/intel_pub.py
@@ -1,6 +1,8 @@
 import rclpy
 from rclpy.node import Node
-from sensor_msgs.msg import Image
+from sensor_msgs.msg import Image, PointCloud2
+from sensor_msgs_py.point_cloud2 import create_cloud
+from sensor_msgs_py.point_cloud2 import PointField
 from cv_bridge import CvBridge
 import cv2
 import pyrealsense2 as rs
@@ -14,6 +16,8 @@ class IntelPublisher(Node):
         super().__init__("Intel_publisher")  
         self.intel_publisher_rgb = self.create_publisher(Image, "rgb_frame", 10)  # Publisher
         self.intel_publisher_bg_removed = self.create_publisher(Image, "bg_removed_frame", 10)
+        self.intel_publisher_pcl = self.create_publisher(PointCloud2, "point_cloud", 10)
+        
         self.br_rgb = CvBridge()  # Convertidor de OpenCV a ROS
         
         try:
@@ -29,12 +33,12 @@ class IntelPublisher(Node):
             
             # Depth Scale
             self.depth_scale = self.pipe.get_active_profile().get_device().first_depth_sensor().get_depth_scale()
-            
-            # Set clipping distance (1 mts)
             self.clipping_distance = 1.0 / self.depth_scale
-            
-            # Depth color
+            # Depth color# Set clipping distance (1 mts)
             self.align = rs.align(rs.stream.color)
+            
+            # Procesar PointCloud
+            self.pc = rs.pointcloud()
             
             # Hilo separado para el procesamiento de frames
             self.camera_thread = threading.Thread(target=self.start_camera, daemon=True)
@@ -72,6 +76,11 @@ class IntelPublisher(Node):
                 bg_removed = self.background_removal(color_image, depth_image)
                 self.intel_publisher_bg_removed.publish(self.br_rgb.cv2_to_imgmsg(bg_removed, encoding='rgb8'))
                 
+                # Generar PC y poblicar Point Cloud 2
+                point_cloud = self.generate_point_cloud(color_frame, depth_frame)
+                if point_cloud :
+                    self.intel_publisher_pcl.publish(point_cloud)
+                
                 self.get_logger().info(f"Publishing RGB frame. Procesado en {time.time() - start_time:.3f} segundos")
         except Exception as e:
             self.get_logger().error(f"Error al capturar frames: {e}")
@@ -88,7 +97,48 @@ class IntelPublisher(Node):
         bg_removed_rgb = cv2.cvtColor(bg_removed.astype(np.uint8), cv2.COLOR_BGR2RGB)
         
         return bg_removed_rgb
+    
+    def generate_point_cloud (self, color_frame, depth_frame): 
+        ''' Convierte Frames a msg PointCloud'''
+        
+        intrinsics = color_frame.profile.as_video_stream_profile().intrinsics
+        height , width = depth_frame.get_height(), depth_frame.get_width()
+        
+        points = []
+        color_image = np.asanyarray(color_frame.get_data())
+        
+        for y in range (0, height, 2): #Procesar cada 2 pixeles
+            for x in range(0, width, 2): 
+                depth = depth_frame.get_distance(x, y)
+                if depth == 0 : # Ignora puntos sin datos 
+                    continue 
+                    
+                # Proyectar pixel a espacio 3D
+                point = rs.rs2_deproject_pixel_to_point(intrinsics, [x , y], depth)
+                
+                # Extraer color a formato RGB
+                b , g, r = color_image[y, x]
+                points.append([point[0], point[1], point[2], r, g, b])
 
+            if not points : 
+                self.get_logger().warning('No hay Point Clouds')
+                return None
+            
+        # Definir campos para PointCloud 2
+            fields = [
+            PointField(name="x", offset=0, datatype=PointField.FLOAT32, count=1),
+            PointField(name="y", offset=4, datatype=PointField.FLOAT32, count=1),
+            PointField(name="z", offset=8, datatype=PointField.FLOAT32, count=1),
+            PointField(name="r", offset=12, datatype=PointField.UINT8, count=1),
+            PointField(name="g", offset=13, datatype=PointField.UINT8, count=1),
+            PointField(name="b", offset=14, datatype=PointField.UINT8, count=1),
+        ]
+        
+        # Crear el mensaje PointCloud2
+        header = self.get_clock().now().to_msg()
+        header.frame_id = "camera_frame"
+        return create_cloud(header, fields, points)
+            
     def destroy_node(self):
         """Detiene el pipeline al cerrar el nodo."""
         self.pipe.stop()
