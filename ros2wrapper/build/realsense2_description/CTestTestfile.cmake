52a01e61569b0039685de982bb72d749700ac963
Ulises-Diaz <A00837223@tec.mx>
2025-02-04 23:21:33 -0600

    Removed embedded Git repository

---
+++ ros2wrapper/build/realsense2_description/CTestTestfile.cmake
@@ -0,0 +1,6 @@
+# CMake generated Testfile for 
+# Source directory: /home/uli/Desktop/tec/personal/intel_sense/ros2wrapper/src/realsense-ros/realsense2_description
+# Build directory: /home/uli/Desktop/tec/personal/intel_sense/ros2wrapper/build/realsense2_description
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
